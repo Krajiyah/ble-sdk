27ff1b57060f14aa378f2fa79222785b881148a6
Krishnan Rajiyah <krajiyah@gmail.com>
2019-10-26 18:05:30 -0700

    feat: add code coverage blocker to PRs

--- scripts/check_cov.sh
+++ scripts/check_cov.sh
@@ -3,6 +3,9 @@
 cov=$(cat report.txt | tail -n 1 | tr -s '\t' | sed 's/\t/ /g' | cut -d ' ' -f 3 | tr -d '%')
 cov=${cov%.*}
 if (($cov < 70)); then
+    echo ""
+    echo ""
+    echo "==========================ERROR=============================="
     echo "$cov% is less than required 70% code coverage. Please write tests."
     exit 1
 fi
