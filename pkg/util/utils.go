69e8550220ea709551b95162cfe1e2fbf6d82cba
Krishnan Rajiyah <krajiyah@gmail.com>
2019-12-01 22:43:35 -0800

    fix: char uuid string method broken

--- pkg/util/utils.go
+++ pkg/util/utils.go
@@ -17,6 +17,20 @@ func AddrEqualAddr(a string, b string) bool {
 	return strings.ToUpper(a) == strings.ToUpper(b)
 }
 
+func insertStr(src string, chars string, index int) string {
+	return src[:index] + chars + src[index:]
+}
+
+func UuidToStr(u ble.UUID) string {
+	delim := "-"
+	src := strings.ToUpper(u.String())
+	a := insertStr(src, delim, 8)
+	b := insertStr(a, delim, 13)
+	c := insertStr(b, delim, 18)
+	d := insertStr(c, delim, 23)
+	return d
+}
+
 func UuidEqualStr(u ble.UUID, s string) bool {
 	compare := strings.Replace(s, "-", "", -1)
 	return AddrEqualAddr(compare, u.String())
